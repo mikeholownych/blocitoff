d224e303b376bf5cba46f6289e6e200cff2af305
Mike Holownych <mike.holownych@gmail.com>
2014-06-24 04:45:48 +0000

    RSpec Implemented for User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
-require 'rails_helper'
+require 'spec_helper'
 
-RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User do
+  it 'should be able to sign up with a username, password, and email address'
+  it 'should be able to sign in and out'
+  it 'should be able to create a single to-do list'
+  it 'should be abel to create multiple to-do items on its own to-do list'
+  it 'should be able to mark to-do items as complete'
+  it 'should be able to see how many days are left before a to-do item is automatically deleted'
 end
