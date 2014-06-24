9c3f159ffde8d946b9b2f1e08f0f997cf363f2d7
Mike Holownych <mike.holownych@gmail.com>
2014-06-24 14:55:22 +0000

    setup User tests

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,16 @@
 
 FactoryGirl.define do
   factory :user do
+    sequence(:name) { |i| "Mike#{i}" }
+    sequence(:email) { |i| "testuser#{i}@domain.com" }
+    password "password"
+    password_confirmation "password"
+
+    factory :ben do
+      name "Mike"
+      email "mike@test.com"
+      password "password"
+      password_confirmation "password"
+    end
   end
 end
