317e509a3f68d9eff1cee0e2f1d8a0e0b758a92a
Mike Holownych <mike.holownych@gmail.com>
2014-06-27 12:02:44 +0000

    Added scaffold for lists

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Blocitoff::Application.routes.draw do
 
+  resources :lists
+
   devise_for :users
 
   resources :users, only: [:show, :index, :update]
 
-  get '/about' => 'welcome#about'
-
+  get '/about' => "welcome#about"
+  
   root to: 'welcome#index'
 
 end
