0a457ad47df288669df0265baa1853245fb2c821
Mike Holownych <mike.holownych@gmail.com>
2014-06-27 15:38:59 +0000

    Added Items Controller

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,10 @@
+class ItemsController < ApplicationController
+  def show
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -52,7 +52,6 @@ ActiveRecord::Schema.define(version: 20140627130954) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
